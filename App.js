136d6f59d6ec2f557b9196d0fb60ef7dbbd07dea
marciobnts <marciobnts@gmail.com>
2025-05-06 08:05:05 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Vigilância Sanitária de Jaraguari-MS</h1>
+      <p>Site em desenvolvimento com funcionalidades de gestão de alvarás.</p>
+    </div>
+  );
+}
+
+export default App;
